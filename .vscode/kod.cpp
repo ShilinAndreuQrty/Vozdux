6eba48884c3e6453b54f94572de172499ba2eb2f
Fawol52 <valeraewe@gmail.com>
2026-04-10 19:57:58 +0300

    1

--- .vscode/kod.cpp
+++ .vscode/kod.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 #include <windows.h>
 #include <random>
+#include <string>
+#include <locale>
 
 using namespace std;
 
-int k1[]{1,3,6,2,4,5};
+int k1[]{1,3,6,2,4,5,7,8};
 int k2[]{3,1,2,4};
 char s[]{"А ты лох"};
-char *s1[]{new char[10]};
-char *s2[]{new char[10]};
-char *s3[]{new char[10]};
-char *s4[]{new char[10]};
+char *s1{new char[10]};
+char *s2{new char[10]};
+char *s3{new char[10]};
+char *s4{new char[10]};
 char alph[]{"йцукенгшщзхъфывапролджэячсмитьбюё"};
+string r;
+
+void fe(char *mas){
+    for (int i=0;mas[i];i++){
+        
+    }
+}
 
 int main(){
+    setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     unsigned seed = time(0);
     mt19937 gen (seed);
-    cout << "в" << endl;
-    return 0;
+    fe(s1);
+    for (int i=0;s1[i];i++){
+        cout << s1[i] << " ";
+    }
 }
